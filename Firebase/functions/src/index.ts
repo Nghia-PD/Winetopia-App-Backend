ef9cf37d147df6202640ad107cb9dc09dc6ec049
Nghiax17 <Nghia.PD@outlook.com>
2025-04-22 17:54:19 +1200

    [FEAT] handling ticket information changing

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -44,11 +44,12 @@ const createNewUserRecord = async (uid: string, email: string, first_name: strin
     }
 }
 
-const createNewUserAuth = async (email: string, first_name: string, last_name: string, phone: string, ticket_type: string, ticket_number: string) => {
+const createNewUserAuth = async (password: string, email: string, first_name: string, last_name: string, phone: string, ticket_type: string, ticket_number: string) => {
     try{
         const authRecord = await admin.auth().createUser({
-            email,
-            password: "Winetopia2025",
+            uid: ticket_number,
+            email: email,
+            password: "Winetopia2025", // change this to variable in the furture.
         });
         logger.info("✅ User created:", authRecord.email);
         await createNewUserRecord(authRecord.uid, email, first_name, last_name, phone, ticket_type, ticket_number);
@@ -65,6 +66,49 @@ const createNewUserAuth = async (email: string, first_name: string, last_name: s
     }
 }
 
+const overrideCurrentUser = async (email: string, first_name: string, last_name: string, phone: string, ticket_number: string) => {
+    try{
+        const authRecord = await admin.auth().getUser(ticket_number);
+        if(email != authRecord.email){
+            await admin.auth().updateUser(ticket_number, {
+                email: email,
+            });
+        }
+
+        await admin.firestore().collection('users').doc(ticket_number).update({
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
+            phone: phone,
+        });
+        
+    }catch (error: any){
+        if(error.code === "auth/email-already-exists"){
+            logger.warn("⚠️ Other user already exists with email:", email);
+        }
+        else{
+            logger.error("Error override user: ", error);
+        }
+        //call send email function, pass in the error
+    }
+}
+
+const checkExistingTicket = async (ticket_number: string) => {
+    try {
+        await admin.auth().getUser(ticket_number);
+        logger.info("The ticket exist and will be override with the new payload");
+        return true;
+    } catch (error: any) {
+        if(error.code === "auth/user-not-found"){
+            console.log("User not found, this could be a new ticket");
+            return false;
+        }
+        else{
+            console.log("Error: ", error);
+            return null;
+        }
+    }
+}
 
 function checkEventId(event_id: string): boolean {
     return event_id === winetopia2025_event_id;
@@ -85,6 +129,7 @@ export const flicketWebhookHandler = functions.https.onRequest(
         }
         
         const event_id = req.body?.event_id ?? null;
+        const event_name = req.body?.even_name ?? "Event2025";
         const ticket_holder_details = req.body?.ticket_holder_details ?? null;
         const ticket_holder_email = ticket_holder_details?.email ?? null;
         const ticket_type = req.body?.ticket_type ?? null;
@@ -111,7 +156,13 @@ export const flicketWebhookHandler = functions.https.onRequest(
         }
 
         else{
-            await createNewUserAuth(ticket_holder_email, ticket_holder_details.first_name, ticket_holder_details.last_name, ticket_holder_details.cell_phone, ticket_type, ticket_number);
+            const userExistFlag = await checkExistingTicket(ticket_number);
+            if(userExistFlag === true){
+                await overrideCurrentUser(ticket_holder_email, ticket_holder_details.first_name, ticket_holder_details.last_name, ticket_holder_details.cell_phone, ticket_number);
+            }
+            else if(userExistFlag === false){
+                await createNewUserAuth(event_name, ticket_holder_email, ticket_holder_details.first_name, ticket_holder_details.last_name, ticket_holder_details.cell_phone, ticket_type, ticket_number);
+            }
         }
 
         res.status(200).end();
