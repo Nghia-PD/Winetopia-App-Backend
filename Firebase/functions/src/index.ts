229509c17d416cbe9ef8de25c13508d6f42d2fed
Nghiax17 <Nghia.PD@outlook.com>
2025-04-26 17:17:38 +1200

    Rewrite fucntions for new database structure

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -3,12 +3,74 @@
 import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
+import { DocumentSnapshot } from "firebase-admin/firestore";
 
 const winetopia2025_event_id = "39d1bce3-ad5b-41fb-8a41-aadee4d239b9";
+const ticket_collection = "2025_tickets";
 
 admin.initializeApp();
 
-const createNewUserRecord = async (uid: string, email: string, first_name: string, last_name: string, phone: string, ticket_type: string, ticket_number: string) => {
+const createNewTicketRecord = async (ticket_number: string, ticket_type: string, ticket_holder_email: string, order_number: string) => {
+    try {
+        await admin.firestore().collection(ticket_collection).doc(ticket_number).set({
+            ticket_number: ticket_number,
+            ticket_holder_email: ticket_holder_email,
+            ticket_type: ticket_type,
+            order_number: order_number,
+        });
+        logger.info("✅ Create a new ticket record in Firestore with ticket id: ", ticket_number);
+    } catch (error) {
+        logger.error("Error create a new ticket in Firestore: ", error);
+    }
+}
+
+
+// TODO split upgrading ticket type and change email on ticket into 2 functions.
+const editTicketDetails = async (target_ticket: DocumentSnapshot, ticket_number: string, ticket_type: string, email: string, order_number: string) => {
+    try {
+        const current_email_on_ticket = await target_ticket.get("ticket_holder_email");
+        const current_ticket_type = await target_ticket.get("ticket_type");
+        const current_attendee = await admin.firestore().collection("Users").doc(current_email_on_ticket).get();
+        const silver_token = await current_attendee.get("silver_token");
+        const gold_token = await current_attendee.get("gold_token");
+        
+        // Changing email on ticket
+        if(current_email_on_ticket != email){
+            // TODO: deload
+            await admin.firestore().collection("Users").doc(current_email_on_ticket).update({
+                silver_token: parseInt(silver_token) - 5,
+                gold_token: parseInt(gold_token) - 1,
+            });
+
+            // TODO: create new auth
+
+            // TODO: create new user record
+
+            // Update the ticket info.
+            await admin.firestore().collection(ticket_collection).doc(ticket_number).update({
+                ticket_holder_email: email,
+                order_number: order_number 
+            });
+        }
+            
+        // Upgrading ticket type - Flicket support upgrading ticket only!
+        if(current_ticket_type.toLowerCase() == "standard" && ticket_type.toLocaleLowerCase() == "premium"){
+            
+            await admin.firestore().collection("Users").doc(email).update({
+                silver_token: parseInt(silver_token) + 5,
+                gold_token: parseInt(gold_token) + 1
+            })
+            await admin.firestore().collection(ticket_collection).doc(ticket_number).update({
+                ticket_type: ticket_type // Update ticket type
+            });
+        }
+    }
+    catch (error) {
+        logger.error("Error edit ticket " + ticket_number + " : " + error);
+    }
+}
+
+const createNewUserRecord = async (email: string, first_name: string, last_name: string, phone: string, ticket_type: string, ticket_number: string) => {
     let silver_token: number;
     let gold_token: number;
 
@@ -28,7 +90,7 @@ const createNewUserRecord = async (uid: string, email: string, first_name: strin
     }
 
     try {
-        await admin.firestore().collection('users').doc(uid).set({
+        await admin.firestore().collection('users').doc(email).set({
             email: email,
             first_name: first_name,
             last_name: last_name,
@@ -38,21 +100,21 @@ const createNewUserRecord = async (uid: string, email: string, first_name: strin
             silver_token: silver_token,
             gold_token: gold_token,
         });
-        logger.info("✅ User record created in Firestore for UID:", uid);
-    } catch (error: any) {
-        logger.error("Error creating user record in Firestore:", error);
+        logger.info("✅ User record created in Firestore for UID:", email);
+    } catch (error) {
+        logger.error("Error creating user record in Firestore: ", error);
     }
 }
 
-const createNewUserAuth = async (password: string, email: string, first_name: string, last_name: string, phone: string, ticket_type: string, ticket_number: string) => {
+const createNewUserAuth = async (email: string, first_name: string, last_name: string, phone: string, ticket_type: string, ticket_number: string) => {
     try{
         const authRecord = await admin.auth().createUser({
-            uid: ticket_number,
+            uid: email,
             email: email,
             password: "Winetopia2025", // change this to variable in the furture.
         });
         logger.info("✅ User created:", authRecord.email);
-        await createNewUserRecord(authRecord.uid, email, first_name, last_name, phone, ticket_type, ticket_number);
+        await createNewUserRecord(email, first_name, last_name, phone, ticket_type, ticket_number);
         return authRecord.uid;;
     }catch (error: any){
         if(error.code === "auth/email-already-exists"){
@@ -95,18 +157,18 @@ const overrideCurrentUser = async (email: string, first_name: string, last_name:
 
 const checkExistingTicket = async (ticket_number: string) => {
     try {
-        await admin.auth().getUser(ticket_number);
-        logger.info("The ticket exist and will be override with the new payload");
-        return true;
-    } catch (error: any) {
-        if(error.code === "auth/user-not-found"){
-            console.log("User not found, this could be a new ticket");
-            return false;
+        const target_ticket = await admin.firestore().collection(ticket_collection).doc(ticket_number).get();
+        if(target_ticket.exists){
+            logger.info("Found an exist ticket under number: ", ticket_number);
         }
         else{
-            console.log("Error: ", error);
-            return null;
+            logger.info("New ticket number: ", ticket_number);
         }
+
+        return target_ticket;
+    } catch (error) {
+        logger.info("Error when trying to access the ticket: ", error);
+        return false;
     }
 }
 
@@ -129,7 +191,6 @@ export const flicketWebhookHandler = functions.https.onRequest(
         }
         
         const event_id = req.body?.event_id ?? null;
-        const event_name = req.body?.even_name ?? "Event2025";
         const ticket_holder_details = req.body?.ticket_holder_details ?? null;
         const ticket_holder_email = ticket_holder_details?.email ?? null;
         const ticket_type = req.body?.ticket_type ?? null;
@@ -156,13 +217,7 @@ export const flicketWebhookHandler = functions.https.onRequest(
         }
 
         else{
-            const userExistFlag = await checkExistingTicket(ticket_number);
-            if(userExistFlag === true){
-                await overrideCurrentUser(ticket_holder_email, ticket_holder_details.first_name, ticket_holder_details.last_name, ticket_holder_details.cell_phone, ticket_number);
-            }
-            else if(userExistFlag === false){
-                await createNewUserAuth(event_name, ticket_holder_email, ticket_holder_details.first_name, ticket_holder_details.last_name, ticket_holder_details.cell_phone, ticket_type, ticket_number);
-            }
+            // TODO: check actions and call nesseary helper fucntion
         }
 
         res.status(200).end();
