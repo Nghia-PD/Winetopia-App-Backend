c966213fe7742e20bb0a5adec7053c66193d90d7
Nghiax17 <Nghia.PD@outlook.com>
2025-05-01 15:03:53 +1200

    [FEAT] Send email to attendee

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -3,8 +3,13 @@
 import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
+import * as fucntionsV1 from "firebase-functions/v1";
+import sgMail from '@sendgrid/mail';
+import * as dotenv from 'dotenv';
 
-const winetopia2025_event_id = "39d1bce3-ad5b-41fb-8a41-aadee4d239b9";
+// Load environment variables from .env file
+dotenv.config();
+const winetopia2025_event_id = process.env.WINETOPIA2025_EVENT_ID;
 
 admin.initializeApp();
 
@@ -44,19 +49,19 @@ const createNewUserRecord = async (ticket_number: string, ticket_type: string, e
     }
 }
 
-const createNewUserAuth = async (ticket_number: string, json_ticket_holder_email: string) => {
+const createNewUserAuth = async (ticket_number: string, json_ticket_holder_email: string, name: string) => {
     try{
         const authRecord = await admin.auth().createUser({
             uid: ticket_number,
             email: json_ticket_holder_email,
             password: "Winetopia2025",
+            displayName: name
         });
         logger.info("✅ User created:", authRecord.email); 
 
         return true;
     }catch (error: any){
         if(error.code === "auth/email-already-exists"){
-            //TODO send email here
             logger.warn("⚠️ User already exists with email:", json_ticket_holder_email);
             return false;
         }
@@ -65,26 +70,6 @@ const createNewUserAuth = async (ticket_number: string, json_ticket_holder_email
             //call send email function, pass in the error
             return error;
         }
-    }
-}
-
-
-// Upgrading ticket type - Flicket support upgrading ticket only!
-const upgradeTicket = async (ticket_number: string, current_ticket_type: string, new_ticket_type: string) => {
-    try {
-        const target_account = await admin.firestore().collection("Users").doc(ticket_number).get();
-        const silver_token = await target_account.get("silver_token");
-        const gold_token = await target_account.get("gold_token");
-        
-        if(current_ticket_type.toLowerCase() == "standard" && new_ticket_type.toLocaleLowerCase() == "premium"){
-            await admin.firestore().collection("Users").doc().update({
-                ticket_type: new_ticket_type,
-                silver_token: parseInt(silver_token) + 5,
-                gold_token: parseInt(gold_token) + 1,
-            })
-        }
-    } catch (error) {
-        logger.error("Error when upgrade ticket type, ticket number: ", ticket_number);
     }
 }
 
@@ -104,6 +89,26 @@ const changeEmailAuth = async (ticket_number: string, json_ticket_holder_email: 
         }
     }
 }
+
+// Upgrading ticket type - Flicket support upgrading ticket only!
+const upgradeTicket = async (ticket_number: string, current_ticket_type: string, new_ticket_type: string) => {
+    try {
+        const target_account = await admin.firestore().collection("Users").doc(ticket_number).get();
+        const silver_token = await target_account.get("silver_token");
+        const gold_token = await target_account.get("gold_token");
+        
+        if(current_ticket_type.toLowerCase() == "standard" && new_ticket_type.toLocaleLowerCase() == "premium"){
+            await admin.firestore().collection("Users").doc().update({
+                ticket_type: new_ticket_type,
+                silver_token: parseInt(silver_token) + 5,
+                gold_token: parseInt(gold_token) + 1,
+            });
+        }
+    } catch (error) {
+        logger.error("Error when upgrade ticket type, ticket number: ", ticket_number);
+    }
+}
+
 const overrideCurrentAccount = async (
     ticket_number: string, 
     json_ticket_type: string,
@@ -116,19 +121,19 @@ const overrideCurrentAccount = async (
         const current_user_record = await admin.firestore().collection("Users").doc(ticket_number).get();
         const current_email = current_user_record.get("email");
         const current_ticket_type = current_user_record.get("ticket_type");
+        const full_name = json_ticket_holder_first_name + " " + json_ticket_holder_last_name;
         
         if(current_email != json_ticket_holder_email){
-            changeEmailAuth(ticket_number, json_ticket_holder_email);
-            await admin.firestore().collection("Users").doc(ticket_number).update({
-                email: json_ticket_holder_email
-            });
+            const flag = await changeEmailAuth(ticket_number, json_ticket_holder_email);
+            if(flag){
+                await admin.firestore().collection("Users").doc(ticket_number).update({
+                    email: json_ticket_holder_email
+                });
+            }
         }
 
         if(current_ticket_type.toLowerCase() != json_ticket_type.toLowerCase()){
-            upgradeTicket(ticket_number, current_ticket_type, json_ticket_type);
-            await admin.firestore().collection("Users").doc(ticket_number).update({
-                ticket_type: json_ticket_type
-            });
+            await upgradeTicket(ticket_number, current_ticket_type, json_ticket_type);
         }
 
         await admin.firestore().collection("Users").doc(ticket_number).update({
@@ -137,6 +142,11 @@ const overrideCurrentAccount = async (
             phone: json_ticket_holder_phone
         });
 
+        await admin.auth().updateUser(ticket_number, {
+            displayName: full_name,
+        });
+        
+        logger.info("Update account info:", ticket_number);
     } catch (error) {
         logger.info("Error when override current account", error);
     }
@@ -156,6 +166,33 @@ function checkEventId(event_id: string): boolean {
     return event_id === winetopia2025_event_id;
 }
 
+const notifyEmailHasBeenUsed = async (email: string, full_name: string) => {
+    const apiKey = process.env.SENDGRID_API_KEY;
+    if (!apiKey) {
+        logger.info("Missing Send Grid API key");
+        throw new Error('Missing SENDGRID_API_KEY in environment variables');
+    }
+    sgMail.setApiKey(apiKey);
+    const msg = {
+        to: email,
+        from: "tech@lemongrassproductions.co.nz",
+        templateId: "d-48d4dae10d894e849a7e45d93fb89028",
+        dynamic_template_data: {
+            name: full_name,
+            email: email
+        }
+    };
+    sgMail
+        .send(msg)
+        .then((response) => {
+        logger.info(response[0].statusCode);
+        logger.info(response[0].headers);
+        })
+        .catch((error) => {
+            logger.info(error);
+    });
+}
+    
 export const flicketWebhookHandler = functions.https.onRequest(
     {
         region: "australia-southeast1",
@@ -205,22 +242,58 @@ export const flicketWebhookHandler = functions.https.onRequest(
                     json_ticket_holder_email, 
                     json_ticket_holder_details.first_name,
                     json_ticket_holder_details.last_name,
-                    json_ticket_holder_details.phone,
+                    json_ticket_holder_details.cell_phone,
                 );
             }
             else if(!ticketExistFlag){
-                await createNewUserAuth(json_ticket_number, json_ticket_holder_email);
-                await createNewUserRecord(
-                    json_ticket_number,
-                    json_ticket_type,
-                    json_ticket_holder_email, 
-                    json_ticket_holder_details.first_name, 
-                    json_ticket_holder_details.last_name, 
-                    json_ticket_holder_details.phone, 
-                );
+                const full_name = json_ticket_holder_details.first_name + json_ticket_holder_details.last_name;
+                const success_create_auth = await createNewUserAuth(json_ticket_number, json_ticket_holder_email, full_name);
+                if(success_create_auth)
+                {
+                    await createNewUserRecord(
+                        json_ticket_number,
+                        json_ticket_type,
+                        json_ticket_holder_email, 
+                        json_ticket_holder_details.first_name, 
+                        json_ticket_holder_details.last_name, 
+                        json_ticket_holder_details.cell_phone, 
+                    );
+                }
+                else if(!success_create_auth){
+                    notifyEmailHasBeenUsed(json_ticket_holder_email, full_name);
+                }
+                // TODO: else{ notify Fail SetUp account}
             }
         }
 
         res.status(200).end();
     }
 );
+
+export const welcome = fucntionsV1.region("australia-southeast1").auth.user().onCreate((user) =>{
+    const apiKey = process.env.SENDGRID_API_KEY;
+    if (!apiKey) {
+        logger.info("Missing Send Grid API key");
+        throw new Error('Missing SENDGRID_API_KEY in environment variables');
+    }
+    sgMail.setApiKey(apiKey);
+    //Send email welcome with login details and app link
+    const msg = {
+        to: user.email,
+        from: "tech@lemongrassproductions.co.nz",
+        templateId: "d-715ec7a4d4414d23a294d2b3c5a7f684",
+        dynamic_template_data: {
+            name: user.displayName,
+            email: user.email
+        }
+    };
+    sgMail
+        .send(msg)
+        .then((response) => {
+        logger.info(response[0].statusCode);
+        logger.info(response[0].headers);
+        })
+        .catch((error) => {
+            logger.info(error);
+        });
+});
